5c3c5c00b17ae657bdf132e21b0cf4e21854608e
刘隆超 <779875923@qq.com>
2020-09-29 15:38:52 +0800

    整改指派权限

--- src/main/java/com/sugar/manage/controller/TUserTaskController.java
+++ src/main/java/com/sugar/manage/controller/TUserTaskController.java
@@ -4,6 +4,7 @@ import com.sugar.common.utils.CookieUtils;
 import com.sugar.manage.dao.model.TUser;
 import com.sugar.manage.dao.vo.TUserTask;
 import com.sugar.manage.service.ITUserTaskService;
+import com.sugar.manage.vo.SysResult;
 import com.sugar.util.ResultMessage;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -53,87 +54,59 @@ private ITUserTaskService itUserTaskService;
 
     @RequestMapping("/addEntrustInfo")
     @ResponseBody
-    public ResultMessage addEntrustInfo(TUserTask tUserTask,HttpServletRequest request) {
+    public SysResult addEntrustInfo(TUserTask tUserTask,HttpServletRequest request) {
         String userId = CookieUtils.getCookie(request, "SUGAR_USER_ID");
 
         ResultMessage resultMessage = new ResultMessage();
         if (StringUtils.isBlank(userId)) {
-            resultMessage.setResultMassage("请登录");
-            resultMessage.setSuccess(false);
-            return resultMessage;
+            return SysResult.success("请先登入",null);
         }
 
        String roles = itUserTaskService.getAllTaskNameByProductId(Long.parseLong(userId));
        String[] roleIdArr = roles.split(",");
        boolean flag = Arrays.asList(roleIdArr).contains("9");
        if (!flag) {
-           resultMessage.setResultMassage("没有指派权限");
-           resultMessage.setSuccess(false);
-           return resultMessage;
+           return SysResult.success("没有指派权限",null);
        }
 
         if (StringUtils.isBlank( tUserTask.getTaskPrincipal())) {
-            resultMessage.setResultMassage("负责人为空");
-            resultMessage.setSuccess(false);
-            return resultMessage;
+            return SysResult.success("负责人为空",null);
         }
         if (StringUtils.isBlank( tUserTask.getStartTime())) {
-            resultMessage.setResultMassage("开始时间为空");
-            resultMessage.setSuccess(false);
-            return resultMessage;
+            return SysResult.success("开始时间为空",null);
         }
         if (StringUtils.isBlank( tUserTask.getProjectId())) {
-            resultMessage.setResultMassage("项目id为空");
-            resultMessage.setSuccess(false);
-            return resultMessage;
+            return SysResult.success("项目id为空",null);
         }
         //1.先判断该项目id的最新阶段，如果是被指派过的阶段则不允许新增
         List<String> hs = itUserTaskService.getAllTaskNameByProductId(tUserTask.getProjectId());
 
         if ("53".equals(userId) && hs.contains("1")) {
-            resultMessage.setResultMassage("商机推进阶段已被指派过，无法重复指派");
-            resultMessage.setSuccess(false);
-            return resultMessage;
+            return SysResult.success("商机推进阶段已被指派过，无法重复指派",null);
         }
         if ("33".equals(userId) && hs.contains("1")) {
-            resultMessage.setResultMassage("商机推进阶段已被指派过，无法重复指派");
-            resultMessage.setSuccess(false);
-            return resultMessage;
+            return SysResult.success("商机推进阶段已被指派过，无法重复指派",null);
         }
         if ("80".equals(userId) && hs.contains("2")) {
-            resultMessage.setResultMassage("采购阶段已被指派过，无法重复指派");
-            resultMessage.setSuccess(false);
-            return resultMessage;
+            return SysResult.success("采购阶段已被指派过，无法重复指派",null);
         }
         if ("80".equals(userId) && hs.contains("3")) {
-            resultMessage.setResultMassage("产品阶段已被指派过，无法重复指派");
-            resultMessage.setSuccess(false);
-            return resultMessage;
+            return SysResult.success("产品阶段已被指派过，无法重复指派",null);
         }
         if ("80".equals(userId) && hs.contains("5")) {
-            resultMessage.setResultMassage("运营阶段已被指派过，无法重复指派");
-            resultMessage.setSuccess(false);
-            return resultMessage;
+            return SysResult.success("运营阶段已被指派过，无法重复指派",null);
         }
         if ("20".equals(userId) && hs.contains("4")) {
-            resultMessage.setResultMassage("研发阶段已被指派过，无法重复指派");
-            resultMessage.setSuccess(false);
-            return resultMessage;
+            return SysResult.success("研发阶段已被指派过，无法重复指派",null);
         }
         if ("20".equals(userId) && hs.contains("6")) {
-            resultMessage.setResultMassage("运维阶段已被指派过，无法重复指派");
-            resultMessage.setSuccess(false);
-            return resultMessage;
+            return SysResult.success("运维阶段已被指派过，无法重复指派",null);
         }
        int count = itUserTaskService.insertTUserTask(tUserTask);
         if (count<=0) {
-            resultMessage.setResultMassage("插入失败");
-            resultMessage.setSuccess(false);
-            return resultMessage;
+            return SysResult.success("插入失败",null);
         }
-        resultMessage.setResultMassage("插入成功");
-        resultMessage.setSuccess(false);
-        return resultMessage;
+        return SysResult.success("插入成功",null);
     }
 
 
