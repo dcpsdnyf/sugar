d2c7549c871801069581798093f904cbbb531996
779875923@qq.com <779875923@qq.com>
2020-10-14 08:48:29 +0800

    新增各阶段负责人SQL

--- src/main/resources/db/ddl/20200911-t_sugar_project新增集团字段.sql
+++ src/main/resources/db/ddl/20200911-t_sugar_project新增集团字段.sql
@@ -1,1 +1,13 @@
 ALTER  TABLE  t_sugar_project  ADD   group_name  VARCHAR(128) COMMENT  '集团名称' ;
+
+ALTER  TABLE  t_sugar_project  ADD   business_principal  VARCHAR(128) COMMENT  '商机负责人' ;
+
+ALTER  TABLE  t_sugar_project  ADD   purchase_principal  VARCHAR(128) COMMENT  '采购负责人' ;
+
+ALTER  TABLE  t_sugar_project  ADD   product_principal  VARCHAR(128) COMMENT  '产品负责人' ;
+
+ALTER  TABLE  t_sugar_project  ADD   development_principal  VARCHAR(128) COMMENT  '研发负责人' ;
+
+ALTER  TABLE  t_sugar_project  ADD   operation_principal  VARCHAR(128) COMMENT  '运营负责人' ;
+
+ALTER  TABLE  t_sugar_project  ADD   operation_maintain_principal  VARCHAR(128) COMMENT  '运维负责人' ;
