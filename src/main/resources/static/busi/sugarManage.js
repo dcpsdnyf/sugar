f529e6f74bd72d3354947e30862d2e638ffc3981
HouTianYun <houtianyun@126.com>
2020-10-15 19:47:49 +0800

    代码提交

--- src/main/resources/static/busi/sugarManage.js
+++ src/main/resources/static/busi/sugarManage.js
@@ -100,8 +100,7 @@ $(function () {
 				formatter: function (value, row, index) {//这里的三个参数：value表示当前行当前列的值；row表示当前行的数据；index表示当前行的索引（从0开始）。
 					var html = '';
 					if (row.appoint) {
-						html = '<div style=\'width:150px;\'><button type="button" onclick="editModel(' + row.id + ')" class="btn btn-primary"  style="font-weight:150;font-size:12px;padding:3px 8px"><span class="glyphicon glyphicon-pencil" aria- hidden="true" ></span >编辑</button >&nbsp;&nbsp;' +
-							'<button type="button" onclick="deleteModel(' + row.id + ')" class="btn btn-danger"  style="font-weight:150;font-size:12px;padding:3px 8px"><span class="glyphicon glyphicon-remove" aria- hidden="true" ></span >删除</button >&nbsp;&nbsp;' +
+						html = '<div style=\'width:150px;\'>' +
 							'<button type="button" onclick="appointModel(' + row.id + ')" class="btn btn-primary"  style="font-weight:150;font-size:12px;padding:3px 8px;margin-top: 10px"><span class="glyphicon glyphicon-pencil" aria- hidden="true" ></span >指派</button >';
 						html += "</div>";
 					} else if (row.delay) {
